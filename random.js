9e1018b362a4d2cd01987a08b3c08f769476ad55
ollieOsh <sosinusi@gmail.com>
2017-04-28 12:35:32 -0500

    aliases are messing up my commits

--- random.js
+++ random.js
@@ -1,7 +1,7 @@
 var arrow = document.getElementById('arrow');
 
 document.onclick = function(event) {
-	arrow.innerHTML = `<img src="https://image.freepik.com/free-icon/curve-arrow-point-to-left_318-10099.jpg"><p>Hi</p>`;
+	arrow.innerHTML = `<img src="https://image.freepik.com/free-icon/curve-arrow-point-to-left_318-10099.jpg">`;
 
 	var toRight = Math.ceil(Math.random() * 10);
 
